4b8d97f3c9aee04038b8d386e0304b35f00b5ace
NYeonK <skdus728@gmail.com>
2022-06-30 05:38:58 +0900

    [feat] 게임 초기화

---
+++ kingman/game.js
@@ -0,0 +1,10 @@
+class Hangman {
+  constructor(word, remainingGuesses) {
+    this.word = word.toLowerCase().split('')
+    this.remainingGuesses = remainingGuesses
+    this.guessedLetters = []
+    this.status = 'playing'
+  }
+}
+
+export default Hangman;
